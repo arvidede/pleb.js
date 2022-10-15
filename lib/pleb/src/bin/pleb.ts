0a8c017e01d1f4ce9e002e2747b2dfc9328f9b5e
Arvid Edenheim <arvidede@icloud.com>
2022-10-15 19:37:16 +0200

    Hide experimental node warnings

--- lib/pleb/src/bin/pleb.ts
+++ lib/pleb/src/bin/pleb.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --experimental-specifier-resolution=node
+#!/usr/bin/env node --no-warnings --experimental-specifier-resolution=node
 import { program } from 'commander'
 import dev from '../cli/dev'
 import start from '../cli/start'
